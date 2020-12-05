bda21359eab1b6903801ae923507155b805a4dbc
pavelperc <pavelperc@mail.ru>
2020-12-05 14:42:20 +0300

    updated package name again

--- app/src/androidTest/java/com/flamingo/dietapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/flamingo/dietapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.dietapp", appContext.packageName)
+        assertEquals("com.flamingo.dietapp", appContext.packageName)
     }
 }
