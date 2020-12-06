0684a84e12522a97e41a1768b561e19179526e20
pavelperc <pavelperc@mail.ru>
2020-12-06 03:05:25 +0300

    Some utils.

--- app/src/main/java/com/flamingo/dietapp/utils/commonUtils/CommonUtils.kt
+++ app/src/main/java/com/flamingo/dietapp/utils/commonUtils/CommonUtils.kt
@@ -1,6 +1,7 @@
 package com.flamingo.dietapp.utils.commonUtils
 
 import android.content.Context
+import android.util.TypedValue
 import android.widget.Toast
 import org.json.JSONArray
 
@@ -8,3 +9,5 @@ fun Context.toast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT)
 fun Context.longToast(text: String) = Toast.makeText(this, text, Toast.LENGTH_LONG).show()
 
 fun JSONArray.toList() = List(length()) { i -> getJSONObject(i) }
+
+fun Context.dip(value: Int) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value.toFloat(), resources.displayMetrics).toInt()
